d87718cc60a42440bf03de01ec57f9570c4172be
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2023-12-21 21:20:25 +0300

    update

--- avg.py
+++ avg.py
@@ -1,17 +1,22 @@
 import random
-a=100
-toplam=0
-ortalama=0
-kume =[]
-kume1=[]
-while(a>0):
-    sayi=int(random.randint(0, 1000))
-    kume.append(sayi)
-    a =a-1
-for i in range(0,100,1):
-    toplam +=kume[i]
-    if(kume[i]<100):
-        kume1.append(kume[i])
-    ortalama=toplam/100
-print(kume1)
-print(ortalama)
+b=0
+while 1:
+    a=100
+    toplam=0
+    ortalama=0
+    kume =[]
+    kume1=[]
+    while(a>0):
+        sayi=int(random.randint(0, 1000))
+        kume.append(sayi)
+        a =a-1
+    for i in range(0,100,1):
+        toplam +=kume[i]
+        ortalama=toplam/100
+    print(ortalama)
+    if(ortalama==500):
+        print(b,"Deneme",ortalama)
+        break
+    else:
+        b =b+1
+
